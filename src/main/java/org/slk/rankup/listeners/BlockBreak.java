6557887d3ca72d1aaa11ad009fbd3b9e8a0824f6
guilherme.fadario <guilherme.fadario@medicare.pt>
2023-06-07 16:53:47 +0100

    IPlaceable

--- src/main/java/org/slk/rankup/listeners/BlockBreak.java
+++ src/main/java/org/slk/rankup/listeners/BlockBreak.java
@@ -4,14 +4,23 @@ import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.slk.rankup.interfaces.IPlaceable;
 import org.slk.rankup.managers.PlaceableManager;
+
+import java.util.Map;
 
 public class BlockBreak implements Listener {
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event){
         Location location = event.getBlock().getLocation();
         if(PlaceableManager.getMap().containsValue(location)){ // placeable
-
+            IPlaceable placeable = null;
+            for(Map.Entry<IPlaceable, Location> placb : PlaceableManager.getMap().entrySet()){
+                if(placb.getValue() == location) { placeable = placb.getKey(); }
+            }
+            assert placeable != null;
+            PlaceableManager.removePlaceable(placeable);
+            placeable.onDestroyed();
         }
     }
 }
