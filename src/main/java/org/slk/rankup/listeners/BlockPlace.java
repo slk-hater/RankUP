8223f9426d3a1b0c9e641dc8ffaac3f100552d28
guilherme.fadario <guilherme.fadario@medicare.pt>
2023-06-07 16:35:00 +0100

    IPlaceable

--- src/main/java/org/slk/rankup/listeners/BlockPlace.java
+++ src/main/java/org/slk/rankup/listeners/BlockPlace.java
@@ -3,9 +3,16 @@ package org.slk.rankup.listeners;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+import org.slk.rankup.interfaces.IPlaceable;
+import org.slk.rankup.managers.PlaceableManager;
 
 public class BlockPlace implements Listener {
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event){
+        ItemStack is = event.getItemInHand();
+        if(is.getItemMeta().getCustomModelData() == 1){ // is placeable
+            PlaceableManager.addPlaceable((IPlaceable) is, event.getBlockPlaced().getLocation());
+        }
     }
 }
