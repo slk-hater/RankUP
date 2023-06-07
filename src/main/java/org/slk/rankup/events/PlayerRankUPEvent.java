34b2c5c10658b6a0f6e99190464f1f2b4e4534dc
guilherme.fadario <guilherme.fadario@medicare.pt>
2023-06-07 11:46:56 +0100

    RankUP event

---
+++ src/main/java/org/slk/rankup/events/PlayerRankUPEvent.java
@@ -0,0 +1,25 @@
+package org.slk.rankup.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.slk.rankup.ranks.Rank;
+
+public class PlayerRankUPEvent extends Event {
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+    public static HandlerList getHandlerList() { return HANDLER_LIST; }
+
+    private final Player player;
+    private final Rank rank;
+
+    public PlayerRankUPEvent(Player player, Rank rank) {
+        this.player = player;
+        this.rank = rank;
+    }
+
+    @Override
+    public HandlerList getHandlers() { return HANDLER_LIST; }
+    public Player getPlayer() { return player; }
+    public Rank getRank() { return rank; }
+    public double getPrice() { return rank.getPrice(); }
+}
