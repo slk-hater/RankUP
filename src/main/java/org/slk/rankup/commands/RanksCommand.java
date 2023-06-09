9286432853392badeafd3dafc13b7d71758b9636
Guilherme Fadário <teamfirekill75@gmail.com>
2023-06-10 00:24:29 +0100

    🧶 Ranks command implementation

---
+++ src/main/java/org/slk/rankup/commands/RanksCommand.java
@@ -0,0 +1,41 @@
+package org.slk.rankup.commands;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.slk.rankup.ranks.Rank;
+import org.slk.rankup.utils.ChatUtils;
+import org.slk.rankup.utils.ItemStackBuilder;
+import org.slk.rankup.utils.NumberUtils;
+
+public class RanksCommand extends Command {
+    public RanksCommand(){
+        super("ranks");
+    }
+    @Override
+    public boolean execute(CommandSender sender, String s, String[] args) {
+        if(!(sender instanceof Player)){
+            sender.sendMessage(ChatUtils.error("Apenas jogadores podem utilizar este comando!"));
+            return true;
+        }
+        Player player = (Player) sender;
+        Rank rank = Rank.getRank(player);
+
+        Inventory inv = Bukkit.createInventory(player, 9*5, "Ranks");
+        int index = 10;
+        for(Rank rankVal : Rank.values()){
+            inv.setItem(index, ItemStackBuilder.build(Material.COAL_BLOCK, 1,
+                    rankVal.getPrefix(),
+                    ChatColor.of("#85bb65") + "$" + NumberUtils.format((long)rankVal.getPrice())));
+            index++;
+        }
+        inv.setItem(40, ItemStackBuilder.build(Material.ARROW, 1, "&cFechar", ""));
+        player.openInventory(inv);
+
+        return true;
+    }
+}
