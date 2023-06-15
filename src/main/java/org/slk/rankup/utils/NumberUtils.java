8585def8701ac69afc541a325d114a8063ef2ba9
guilherme.fadario <guilherme.fadario@medicare.pt>
2023-06-15 15:28:00 +0100

    :art: Improved structure of code

--- src/main/java/org/slk/rankup/utils/NumberUtils.java
+++ src/main/java/org/slk/rankup/utils/NumberUtils.java
@@ -16,16 +16,15 @@ public class NumberUtils {
     }
 
     public static String format(long value) {
-        //Long.MIN_VALUE == -Long.MIN_VALUE so we need an adjustment here
         if (value == Long.MIN_VALUE) return format(Long.MIN_VALUE + 1);
         if (value < 0) return "-" + format(-value);
-        if (value < 1000) return Long.toString(value); //deal with easy case
+        if (value < 1000) return Long.toString(value);
 
         Map.Entry<Long, String> e = suffixes.floorEntry(value);
         Long divideBy = e.getKey();
         String suffix = e.getValue();
 
-        long truncated = value / (divideBy / 10); //the number part of the output times 10
+        long truncated = value / (divideBy / 10);
         boolean hasDecimal = truncated < 100 && (truncated / 10d) != (truncated / 10);
         return hasDecimal ? (truncated / 10d) + suffix : (truncated / 10) + suffix;
     }
