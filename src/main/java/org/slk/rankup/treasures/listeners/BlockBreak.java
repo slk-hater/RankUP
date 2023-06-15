a837a1762163652762eb44b2217eec0326913feb
guilherme.fadario <guilherme.fadario@medicare.pt>
2023-06-15 12:16:49 +0100

    🎫 Chance to spawn gem in Treasures world implementation

--- src/main/java/org/slk/rankup/treasures/listeners/BlockBreak.java
+++ src/main/java/org/slk/rankup/treasures/listeners/BlockBreak.java
@@ -1,6 +1,9 @@
 package org.slk.rankup.treasures.listeners;
 
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -9,8 +12,16 @@ import org.slk.rankup.treasures.TreasuresManager;
 public class BlockBreak implements Listener {
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event){
+        Player player = event.getPlayer();
         World world = event.getBlock().getWorld();
 
         event.setDropItems(world.equals(TreasuresManager.getWorld()));
+
+        double chance = Math.random();
+        if(chance > 0.2d){ // 80% chance
+            player.getTargetBlock(null, 4).setType(Material.AMETHYST_BLOCK);
+            player.playSound(player, Sound.BLOCK_NOTE_BLOCK_PLING, 0.3f, 1f);
+            // TODO : Gem break check
+        }
     }
 }
