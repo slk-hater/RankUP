1a5c17243ec943809a9e3062f80fcf7b692b4239
guilherme.fadario <guilherme.fadario@medicare.pt>
2023-06-15 14:58:38 +0100

    :art: Improved structure of ChatUtils code

--- src/main/java/org/slk/rankup/utils/ChatUtils.java
+++ src/main/java/org/slk/rankup/utils/ChatUtils.java
@@ -1,7 +1,7 @@
 package org.slk.rankup.utils;
 
 public class ChatUtils {
-    public static String good(String message) { return ColorUtils.colorize("&a&l# &f" + message); }
-    public static String info(String message) { return ColorUtils.colorize("&e&l# &f" + message); }
-    public static String error(String message) { return ColorUtils.colorize("&c&l# &f" + message); }
+    public static String good(String message) { return ColorUtils.colorize("&a&l# &r" + message); }
+    public static String info(String message) { return ColorUtils.colorize("&e&l# &r" + message); }
+    public static String error(String message) { return ColorUtils.colorize("&c&l# &r" + message); }
 }
