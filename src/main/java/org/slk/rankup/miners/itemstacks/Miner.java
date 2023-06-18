da20c667882cab0114636558cbe1558f91e2ef97
letav <teamfirekill75@gmail.com>
2023-06-18 01:22:39 +0100

    ⭐ Improved code structure of Miner class

--- src/main/java/org/slk/rankup/miners/itemstacks/Miner.java
+++ src/main/java/org/slk/rankup/miners/itemstacks/Miner.java
@@ -15,10 +15,11 @@ public class Miner {
     public enum MinerType {BASIC, ADVANCED}
 
     public static ItemStack SKULL = ItemStackBuilder.getSkull(
-            "http://textures.minecraft.net/texture/fc82742b2a17eebdeaad2b95e566a5f0250c3557429f6a39a79b6ab8bf8",
+            "http://textures.minecraft.net/texture/9908235a1cbecc03a72cdf710f4ed519d65b4da62b54a4ef98a7408fcf51b83b",
             UUID.fromString("da05b761-b174-4ef6-b5e3-de5342f41fc8"));
     static int BASE_SPEED = 1;
     static int MAX_SPEED = 150;
+    static String DESCRIPTION = "&7Tipo &f%type%\n&7Velocidade &f{speed}/s\n\n&8Botão direito no chão para posicionar";
 
     MinerType minerType;
     int speed;
@@ -31,6 +32,7 @@ public class Miner {
 
         ItemMeta meta = getItemStack().getItemMeta();
         if(meta == null) return;
+        meta.setDisplayName(ChatColor.of("#f5f190") + "Minion minerador");
         meta.setCustomModelData(CustomModelDataEnum.MINER.get());
         getItemStack().setItemMeta(meta);
     }
@@ -41,21 +43,30 @@ public class Miner {
     public MinerType getType() { return this.minerType; }
     public void setType(MinerType minerType) {
         this.minerType = minerType;
+        String translatedType;
+        if(minerType.equals(MinerType.ADVANCED)) translatedType = "AVANÇADO";
+        else translatedType = "BÁSICO";
+        String blocks = speed == 1 ? "bloco" : "blocos";
+
         ItemMeta meta = getItemStack().getItemMeta();
-        if (meta == null) return;
-        switch (minerType) {
-            case ADVANCED -> meta.setDisplayName(ChatColor.of("#f5f190") + "Minerador avançado");
-            default -> meta.setDisplayName(ChatColor.of("#f5f190") + "Minerador básico");
-        }
-        this.getItemStack().setItemMeta(meta);
+        if(meta == null) return;
+        String lore = DESCRIPTION.replace("%type%", translatedType).replace("%speed%", getSpeed()+blocks);
+        List<String> loreRes = new ArrayList<>(List.of(lore.split("\n")));
+        loreRes.replaceAll(ColorUtils::colorize);
+        meta.setLore(loreRes);
+        getItemStack().setItemMeta(meta);
     }
     public int getSpeed() { return this.speed; }
     public void setSpeed(int speed) {
         this.speed = Math.min(speed, MAX_SPEED);
+        String translatedType;
+        if(minerType.equals(MinerType.ADVANCED)) translatedType = "AVANÇADO";
+        else translatedType = "BÁSICO";
+        String blocks = speed == 1 ? "bloco" : "blocos";
+
         ItemMeta meta = getItemStack().getItemMeta();
         if(meta == null) return;
-        String blocks = speed == 1 ? "bloco" : "blocos";
-        String lore = "&7Velocidade &f" + speed + " " + blocks + "/s\n\n&8Botão direito no chão para posicionar";
+        String lore = DESCRIPTION.replace("%type%", translatedType).replace("%speed%", getSpeed()+blocks);
         List<String> loreRes = new ArrayList<>(List.of(lore.split("\n")));
         loreRes.replaceAll(ColorUtils::colorize);
         meta.setLore(loreRes);
