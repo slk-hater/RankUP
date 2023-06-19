2843be08fda934af971f172f36d985b7c4ea2a51
guilherme.fadario <guilherme.fadario@medicare.pt>
2023-06-19 12:18:52 +0100

    :art: Improved code structure of Miners overall

--- src/main/java/org/slk/rankup/miners/inventories/DashboardMinerInventory.java
+++ src/main/java/org/slk/rankup/miners/inventories/DashboardMinerInventory.java
@@ -14,6 +14,11 @@ public class DashboardMinerInventory {
     public static ItemStack ENABLED_ITEMSTACK = ItemStackBuilder.build(Material.RED_STAINED_GLASS_PANE, 1, "&c&lDESLIGAR", "&8Clica para desligar o minerador");
     public static ItemStack DISABLED_ITEMSTACK = ItemStackBuilder.build(Material.GREEN_STAINED_GLASS_PANE, 1, "&a&lLIGAR", "&8Clica para ligar o minerador");
 
+    public static ItemStack UPGRADES_ITEMSTACK = ItemStackBuilder.build(Material.KNOWLEDGE_BOOK, 1, "&eAprimoramentos", "&8Clica para ir para a página de aprimoramentos");
+    public static ItemStack FUEL_ITEMSTACK = ItemStackBuilder.build(Material.CHARCOAL, 1, "&eCombustível", "&8Clica para ir para a página de manuseio de combustível");
+    public static ItemStack RESOURCES_ITEMSTACK = ItemStackBuilder.build(Material.CHEST_MINECART, 1, "&eRecolher recursos", "&7Minérios (0/500)\n&7Gemas (0/100)\n\n&8Clica para recolher os recursos");
+    public static ItemStack DESTRUCTION_ITEMSTACK = ItemStackBuilder.build(Material.BARRIER, 1, "&cRecolher minerador", "&8Clica para recolher o minerador");
+
     static Inventory inv = Bukkit.createInventory(null, SIZE, NAME);
     static{
         for(int i=0; i<SIZE; i++)
@@ -21,10 +26,10 @@ public class DashboardMinerInventory {
         //https://minecraft-heads.com/custom-heads/decoration/17952-spruce-crate
         //https://minecraft-heads.com/custom-heads/blocks/23062-coal-ore
         inv.setItem(0, DISABLED_ITEMSTACK);
-        inv.setItem(10, ItemStackBuilder.build(Material.KNOWLEDGE_BOOK, 1, "&eAprimoramentos", "&8Clica para ir para a página de aprimoramentos"));
-        inv.setItem(12, ItemStackBuilder.build(Material.CHARCOAL, 1, "&eCombustível", "&8Clica para ir para a página de manuseio de combustível"));
-        inv.setItem(14, ItemStackBuilder.build(Material.CHEST_MINECART, 1, "&eRecolher recursos", "&7Minérios (0/500)\n&7Gemas (0/100)\n\n&8Clica para recolher os recursos"));
-        inv.setItem(16, ItemStackBuilder.build(Material.BARRIER, 1, "&cRecolher minerador", "&8Clica para recolher o minerador"));
+        inv.setItem(10, UPGRADES_ITEMSTACK);
+        inv.setItem(12, FUEL_ITEMSTACK);
+        inv.setItem(14, RESOURCES_ITEMSTACK);
+        inv.setItem(16, DESTRUCTION_ITEMSTACK);
     }
 
     public static Inventory cloneInventory() {
