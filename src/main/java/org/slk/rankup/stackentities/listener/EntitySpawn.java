52e42b6f2ba36105524c671dee775a5d6ec7444b
guilherme.fadario <guilherme.fadario@medicare.pt>
2023-06-22 11:36:50 +0100

    :art: Improved code structure of EntitySpawn listener class

--- src/main/java/org/slk/rankup/stackentities/listener/EntitySpawn.java
+++ src/main/java/org/slk/rankup/stackentities/listener/EntitySpawn.java
@@ -3,6 +3,7 @@ package org.slk.rankup.stackentities.listener;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Mob;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
@@ -13,6 +14,7 @@ public class EntitySpawn implements Listener {
     @EventHandler
     public void onEntitySpawn(EntitySpawnEvent event){
         Entity entity = event.getEntity();
+        if(!(entity instanceof Mob)) return;
         Location location = event.getLocation();
 
         for(Entity nearbyEnt : location.getWorld().getNearbyEntities(location, StackEntitiesManager.RADIUS, StackEntitiesManager.RADIUS, StackEntitiesManager.RADIUS)){
