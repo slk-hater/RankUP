3209a02d4c4dd9acc922ff88e2228db527a144e1
guilherme.fadario <guilherme.fadario@medicare.pt>
2023-06-22 12:54:29 +0100

    :lipstick: Update TreasuresMessages

--- src/main/java/org/slk/rankup/treasures/TreasuresMessages.java
+++ src/main/java/org/slk/rankup/treasures/TreasuresMessages.java
@@ -8,7 +8,7 @@ public enum TreasuresMessages {
     TIME_LEFT("Restam-te apenas &e%timeLeft% minutos&r no mundo de Tesouros!"),
     LEAVE_WORLD_TIME("O teu tempo no mundo dos Tesouros acabou!"),
     LEAVE_WORLD_OPTIONAL("Sais-te do mundo de Tesouros ainda com tempo restante, recebeste uma passagem nova com a duração restante."),
-    LEAVE_WORLD_OPTIONAL_NO_TICKET("Sais-te do mundo de Tesouros opcionalmente, como não te restava muito tempo, não vais receber uma passagem nova."),
+    LEAVE_WORLD_OPTIONAL_NO_TICKET("Sais-te do mundo de Tesouros opcionalmente, como não te restava muito tempo, há uma penalização e não vais receber uma passagem nova."),
     LEAVE_WORLD_FORCE("Foste forçado a sair do mundo de Tesouros pelo Servidor, não te preocupes, recebeste uma passagem nova com a duração restante.");
 
     private final String message;
