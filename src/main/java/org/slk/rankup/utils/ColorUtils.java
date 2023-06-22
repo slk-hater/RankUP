1c5071edb4ebd20cc7365561f4b8dc12157fea64
guilherme.fadario <guilherme.fadario@medicare.pt>
2023-06-22 15:31:30 +0100

    :rocket: Deploy ColorUtils.java class

--- src/main/java/org/slk/rankup/utils/ColorUtils.java
+++ src/main/java/org/slk/rankup/utils/ColorUtils.java
@@ -1,7 +1,9 @@
 package org.slk.rankup.utils;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 
 public class ColorUtils {
     public static String colorize(String message){ return ChatColor.translateAlternateColorCodes('&', message); }
+    public static void colorize(Player player, String message){ player.sendMessage(colorize(message)); }
 }
